2667f8022b23ca23cb7a349c902c21b088581e8d
Luke Sanderson <cactuar18@hotmail.com>
2017-01-18 19:12:52 +0000

    Initial Lines
    Here we go!

---
+++ Assets/Line.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour {
+
+    public Vector3[] nodes;
+    public GameObject nodePrefab;
+
+    void Start()
+    {
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            Instantiate(nodePrefab, nodes[i] ,Quaternion.identity);
+        }
+    }
+
+    public void Reset()
+    {
+        nodes = new Vector3[]
+        {
+            new Vector3(1,0,0),
+            new Vector3(2,0,0),
+            new Vector3(3,0,0)
+
+        };
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
